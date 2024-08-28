69a6b412b906d2fbebdf82432485e05c023c75d5
Даніель Білодід <danielbeloded@gmail.com>
2024-08-28 17:12:35 +0300

    working on hover effect on the movie item

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -34,7 +34,7 @@ const Trending = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
   };
@@ -106,13 +106,48 @@ const Trending = () => {
             <div key={movie.id}>
               <h3 className="trending__movie-title">{movie.title}</h3>
               {trailers.length > 0 ? (
-                <div>
+                <div className="trending__movie-thumbnail-container">
                   <img
                     className="trending__movie-thumbnail"
                     src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
                     alt={`${movie.title} thumbnail`}
                     onClick={() => handlePlayVideo(index)}
                   />
+                  <div className="trending__movie-thumbnail-overlay">
+                    <span className="trending__movie-thumbnail-overlay-text">
+                      Play Trailer
+                    </span>
+
+                    <div className="trending__movie-thumbnail-wrapper">
+                      <div className="trending__movie-thumbnail-wrapper-info">
+                        <div className="trending__thumbnail-movie-year">
+                          {release_year}
+                        </div>
+
+                        <div className="trending__movie-thumbnail-dot">·</div>
+
+                        <div className="trending__movie-thumbnail-svg">
+                          <svg
+                            width="20"
+                            height="20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+                              fill="#FFFFFF"
+                            />
+                          </svg>
+                        </div>
+                        <div className="trending__movie-thumbnail-dot">·</div>
+                        <div className="trending__movie-thumbnail-type">
+                          Movie
+                        </div>
+                      </div>
+                      <div className="trending__movie-thumbnail-overlay-name">
+                        {movie.title}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               ) : (
                 <p>No trailer</p>
@@ -144,8 +179,27 @@ const Trending = () => {
               allowFullScreen
             ></iframe>
             <div className="trending__movie-info">
-              <div className="trending__movie-year">
-                {trailers[playVideo].release_year}
+              <div className="trending__movie-wrapper">
+                <div className="trending__movie-year">
+                  {trailers[playVideo].release_year}
+                </div>
+
+                <div className="trending__movie-dot">·</div>
+
+                <div className="trending__movie-svg">
+                  <svg
+                    width="20"
+                    height="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+                      fill="#FFFFFF"
+                    />
+                  </svg>
+                </div>
+                <div className="trending__movie-dot">·</div>
+                <div className="trending__movie-type">Movie</div>
               </div>
 
               <div>
