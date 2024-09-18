63ad076f3b56187692ec78a00101d75806cf364f
Daniel <danielbeloded@gmail.com>
2024-09-18 12:06:47 +0300

    fixed the bookmark bug

--- src/components/bookmark/Bookmark.jsx
+++ src/components/bookmark/Bookmark.jsx
@@ -28,7 +28,7 @@ const Bookmarks = () => {
     handleNextTrailer,
     handlePrevTrailer,
   } = useMovieTrailers(movies);
-  console.log(movies);
+
   const handlePlayTrailer = (index) => {
     setPlayVideo(index);
   };
@@ -53,6 +53,7 @@ const Bookmarks = () => {
     try {
       const moviePromises = bookmarksList.map(async (bookmark) => {
         const movie = await fetchMovieById(bookmark.id);
+        // console.log("Fetched movie:", movie.videos.results[0]); // Проверь, что возвращаемые данные содержат `videos`
         return movie;
       });
       const movies = await Promise.all(moviePromises);
@@ -149,61 +150,58 @@ const Bookmarks = () => {
       </ul>
 
       <Modal isOpen={playVideo !== null} onClose={handleCloseModal}>
-        {playVideo !== null &&
-          trailers[playVideo]?.trailers?.length > 0 &&
-          trailers[playVideo].currentTrailerIndex !== undefined && (
-            <>
-              <iframe
-                className="trending__movie-frame"
-                width="560"
-                height="315"
-                src={`https://www.youtube.com/embed/${
-                  trailers[playVideo].trailers[
-                    trailers[playVideo].currentTrailerIndex
-                  ]?.key || ""
-                }`}
-                title={
-                  trailers[playVideo].trailers[
-                    trailers[playVideo].currentTrailerIndex
-                  ]?.name || "Trailer"
-                }
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-              <div className="trending__movie-info">
-                <div className="trending__movie-wrapper">
-                  <div className="trending__movie-year">
-                    {trailers[playVideo]?.release_year || "Unknown Year"}
-                  </div>
-                  <div className="trending__movie-dot">·</div>
-                  <div className="trending__movie-svg">
-                    <svg
-                      width="20"
-                      height="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
-                        fill="#FFFFFF"
-                      />
-                    </svg>
-                  </div>
-                  <div className="trending__movie-dot">·</div>
-                  <div className="trending__movie-type">Movie</div>
+        {playVideo !== null && movies.length > 0 && (
+          <>
+            <iframe
+              className="trending__movie-frame"
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${
+                movies[playVideo].videos.results[
+                  movies[playVideo].videos.results?.currentTrailerIndex || 0
+                ]?.key || ""
+              }`}
+              title={
+                trailers[playVideo]?.trailers[
+                  trailers[playVideo]?.currentTrailerIndex
+                ]?.name || "Trailer"
+              }
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="trending__movie-info">
+              <div className="trending__movie-wrapper">
+                <div className="trending__movie-year">
+                  {trailers[playVideo]?.release_year || "Unknown Year"}
                 </div>
-
-                <div>
-                  <button onClick={() => handlePrevTrailer(playVideo)}>
-                    previous
-                  </button>
-                  <button onClick={() => handleNextTrailer(playVideo)}>
-                    next
-                  </button>
+                <div className="trending__movie-dot">·</div>
+                <div className="trending__movie-svg">
+                  <svg
+                    width="20"
+                    height="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+                      fill="#FFFFFF"
+                    />
+                  </svg>
                 </div>
+                <div className="trending__movie-dot">·</div>
+                <div className="trending__movie-type">Movie</div>
               </div>
-            </>
-          )}
+              <div>
+                <button onClick={() => handlePrevTrailer(playVideo)}>
+                  previous
+                </button>
+                <button onClick={() => handleNextTrailer(playVideo)}>
+                  next
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </Modal>
     </div>
   );
