99ad66838eda0c4c42a5e95790e459eb7f7d1cc9
Daniel <danielbeloded@gmail.com>
2024-09-24 12:21:13 +0300

    working on search component

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -9,15 +9,18 @@ import { searchMovies } from "../../utils/fetchTrailers";
 
 const SearchResult = () => {
   const data = useSelector((state) => state.data.value);
+
   const {
     trailers,
     playVideo,
+    setPlayVideo,
     handlePlayVideo,
     handleCloseModal,
     handleNextTrailer,
     handlePrevTrailer,
     handleBookmarkClick,
-  } = useMovieTrailers(searchMovies);
+    currentTrailerIndex,
+  } = useMovieTrailers();
 
   return (
     <>
@@ -26,156 +29,135 @@ const SearchResult = () => {
       <div className="popular">
         <div className="popular__text-wrapper"></div>
         <div className="popular__wrapper">
-          {data
-            .slice(0, 10)
-            .map(
-              (
-                { movie, trailers, currentTrailerIndex, release_year },
-                index
-              ) => (
-                <div key={movie.id}>
-                  <div className="trending__btn-wrapper">
-                    <Link
-                      className="trending__info"
-                      to={`/movie-info/${movie.id}`}
-                    >
-                      <FontAwesomeIcon
-                        icon={faInfoCircle}
-                        color="white"
-                        size="1x"
-                      />
-                    </Link>
-                    <div
-                      className="trending__bookmark"
-                      onClick={() => handleBookmarkClick(movie.id)}
-                    >
-                      <FontAwesomeIcon
-                        icon={faBookmark}
-                        color="white"
-                        size="1x"
-                      />
-                    </div>
+          {data.map(
+            ({ movie, trailers, currentTrailerIndex, release_year }, index) => (
+              <div key={movie.id}>
+                <div className="trending__btn-wrapper">
+                  <Link
+                    className="trending__info"
+                    to={`/movie-info/${movie.id}`}
+                  >
+                    <FontAwesomeIcon
+                      icon={faInfoCircle}
+                      color="white"
+                      size="1x"
+                    />
+                  </Link>
+                  <div
+                    className="trending__bookmark"
+                    onClick={() => handleBookmarkClick(movie.id)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faBookmark}
+                      color="white"
+                      size="1x"
+                    />
                   </div>
-                  <h3 className="trending__movie-title">{movie.title}</h3>
-                  {trailers.length > 0 ? (
-                    <div className="trending__movie-thumbnail-container">
-                      <img
-                        className="trending__movie-thumbnail"
-                        src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
-                        alt={`${movie.title} thumbnail`}
-                      />
-                      <div
-                        className="trending__movie-thumbnail-overlay"
-                        onClick={() => handlePlayVideo(index)}
-                      >
-                        <span className="trending__movie-thumbnail-overlay-text">
-                          Play Trailer
-                        </span>
-
-                        <div className="trending__movie-thumbnail-wrapper">
-                          <div className="trending__movie-thumbnail-info">
-                            <div className="trending__thumbnail-movie-year">
-                              {release_year}
-                            </div>
-
-                            <div className="trending__movie-thumbnail-dot">
-                              ·
-                            </div>
-
-                            <div className="trending__movie-thumbnail-svg">
-                              <svg
-                                width="20"
-                                height="20"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
-                                  fill="#FFFFFF"
-                                />
-                              </svg>
-                            </div>
-                            <div className="trending__movie-thumbnail-dot">
-                              ·
-                            </div>
-                            <div className="trending__movie-thumbnail-type">
-                              Movie
-                            </div>
-                          </div>
-                          <div className="trending__movie-thumbnail-overlay-name">
-                            {movie.title}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ) : (
+                </div>
+                <h3 className="trending__movie-title">{movie.title}</h3>
+                {trailers.length > 0 ? (
+                  <div className="trending__movie-thumbnail-container">
                     <img
                       className="trending__movie-thumbnail"
                       src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
                       alt={`${movie.title} thumbnail`}
                     />
-                  )}
-                </div>
-              )
-            )}
+                    <div
+                      className="trending__movie-thumbnail-overlay"
+                      onClick={() => handlePlayVideo(index)}
+                    >
+                      <span className="trending__movie-thumbnail-overlay-text">
+                        Play Trailer
+                      </span>
+
+                      <div className="trending__movie-thumbnail-wrapper">
+                        <div className="trending__movie-thumbnail-info">
+                          <div className="trending__thumbnail-movie-year">
+                            {release_year}
+                          </div>
+
+                          <div className="trending__movie-thumbnail-dot">·</div>
+
+                          <div className="trending__movie-thumbnail-svg">
+                            <svg
+                              width="20"
+                              height="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+                                fill="#FFFFFF"
+                              />
+                            </svg>
+                          </div>
+                          <div className="trending__movie-thumbnail-dot">·</div>
+                          <div className="trending__movie-thumbnail-type">
+                            Movie
+                          </div>
+                        </div>
+                        <div className="trending__movie-thumbnail-overlay-name">
+                          {movie.title}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <img
+                    className="trending__movie-thumbnail"
+                    src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+                    alt={`${movie.title} thumbnail`}
+                  />
+                )}
+              </div>
+            )
+          )}
         </div>
+
         <Modal isOpen={playVideo !== null} onClose={handleCloseModal}>
-          {playVideo !== null &&
-            data.trailers[playVideo] &&
-            data.trailers[playVideo].data.trailers && (
-              <>
-                <iframe
-                  className="trending__movie-frame"
-                  width="560"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${
-                    data.trailers[playVideo].data.trailers[
-                      data.trailers[playVideo].currentTrailerIndex
-                    ].key
-                  }`}
-                  title={
-                    data.trailers[playVideo].data.trailers[
-                      data.trailers[playVideo].currentTrailerIndex
-                    ].name
-                  }
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-                <div className="trending__movie-info">
-                  <div className="trending__movie-wrapper">
-                    <div className="trending__movie-year">
-                      {data.trailers[playVideo].release_year}
-                    </div>
+          {data?.[playVideo]?.trailers && (
+            <>
+              <iframe
+                className="trending__movie-frame"
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${
+                  data[playVideo]?.trailers?.[currentTrailerIndex]?.key || ""
+                }`}
+                title={
+                  data[playVideo]?.trailers?.[currentTrailerIndex]?.name ||
+                  "Trailer"
+                }
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
 
-                    <div className="trending__movie-dot">·</div>
-
-                    <div className="trending__movie-svg">
-                      <svg
-                        width="20"
-                        height="20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
-                          fill="#FFFFFF"
-                        />
-                      </svg>
-                    </div>
-                    <div className="trending__movie-dot">·</div>
-                    <div className="trending__movie-type">Movie</div>
+              <div className="trending__movie-info">
+                <div className="trending__movie-wrapper">
+                  <div className="trending__movie-year">
+                    {data[playVideo]?.release_year}
+                    {console.log(
+                      `https://www.youtube.com/embed/${
+                        data[playVideo].trailers?.[currentTrailerIndex]?.key ||
+                        ""
+                      }`
+                    )}
                   </div>
-
-                  <div>
-                    <button onClick={() => handlePrevTrailer(playVideo)}>
-                      previous
-                    </button>
-                    <button onClick={() => handleNextTrailer(playVideo)}>
-                      next
-                    </button>
-                  </div>
+                  <div className="trending__movie-dot">·</div>
+                  <div className="trending__movie-type">Movie</div>
                 </div>
-              </>
-            )}
+
+                <div>
+                  <button onClick={() => handlePrevTrailer(playVideo)}>
+                    previous
+                  </button>
+                  <button onClick={() => handleNextTrailer(playVideo)}>
+                    next
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </Modal>
       </div>
     </>
