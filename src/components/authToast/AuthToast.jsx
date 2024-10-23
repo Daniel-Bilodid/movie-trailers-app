62b7f57c3d6435708e4bad2da60ee6005333965a
Daniel <danielbeloded@gmail.com>
2024-10-23 12:27:50 +0300

    started to work with motion div

--- src/components/authToast/AuthToast.jsx
+++ src/components/authToast/AuthToast.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./authToast.scss";
+import { motion } from "framer-motion";
 
 const AuthToast = ({ show }) => {
   return (
     show && (
-      <div className="toast">
-        <p>Please auth</p>
-      </div>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+        className="toast"
+      >
+        <p>Please login to bookmark</p>
+      </motion.div>
     )
   );
 };
