cfe5ecf3c6c9947528514c5101c99a89b5b1a9ed
Daniel <danielbeloded@gmail.com>
2024-10-25 13:44:25 +0300

    added some fixes

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -172,7 +172,43 @@ const Trending = () => {
     }, 5000);
   };
   if (load) {
-    return <div>Loading movies...</div>;
+    return (
+      <div className="loading">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="xMidYMid"
+          width="200"
+          height="200"
+          style={{
+            shapeRendering: "auto",
+            display: "block",
+            background: "rgba(255, 255, 255, 0)",
+          }}
+        >
+          <g>
+            <circle
+              strokeDasharray="164.93361431346415 56.97787143782138"
+              r="35"
+              strokeWidth="10"
+              stroke="#5a698f"
+              fill="none"
+              cy="50"
+              cx="50"
+            >
+              <animateTransform
+                keyTimes="0;1"
+                values="0 50 50;360 50 50"
+                dur="1s"
+                repeatCount="indefinite"
+                type="rotate"
+                attributeName="transform"
+              />
+            </circle>
+          </g>
+        </svg>
+      </div>
+    );
   }
 
   return (
