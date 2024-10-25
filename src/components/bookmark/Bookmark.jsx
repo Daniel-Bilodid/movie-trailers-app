2a6a501bc4903906e2a13c5a9cdcc3d2131ca457
Daniel <danielbeloded@gmail.com>
2024-10-25 12:38:07 +0300

    Implemented logic to remove movies from bookmarks

--- src/components/bookmark/Bookmark.jsx
+++ src/components/bookmark/Bookmark.jsx
@@ -11,7 +11,7 @@ import { useBookmarks } from "../../hooks/useBookmarks.js";
 import { setMovies } from "../../redux/store";
 import {
   faInfoCircle,
-  faBookmark,
+  faXmark,
   faPlayCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
@@ -32,6 +32,7 @@ const Bookmarks = () => {
     setPlayVideo,
     handlePlayVideo,
     handleCloseModal,
+    handleBookmarkClick,
   } = useMovieTrailers();
 
   const handlePlayTrailer = (index) => {
@@ -208,8 +209,11 @@ const Bookmarks = () => {
                     size="1x"
                   />
                 </Link>
-                <div className="trending__bookmark">
-                  <FontAwesomeIcon icon={faBookmark} color="white" size="1x" />
+                <div
+                  className="trending__bookmark"
+                  onClick={() => handleBookmarkClick(movie.id)}
+                >
+                  <FontAwesomeIcon icon={faXmark} color="white" size="1x" />
                 </div>
               </div>
               <h3 className="trending__movie-title">
@@ -242,6 +246,30 @@ const Bookmarks = () => {
                           ? movie.first_air_date.slice(0, 4)
                           : "Unknown Year"}
                       </div>
+                      <div className="trending__movie-thumbnail-dot">·</div>
+                      {movie.movieType === "Movie" ? (
+                        <svg
+                          width="20"
+                          height="20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
+                            fill="#FFFFFF"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          width="20"
+                          height="20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M20 4.481H9.08l2.7-3.278L10.22 0 7 3.909 3.78.029 2.22 1.203l2.7 3.278H0V20h20V4.481Zm-8 13.58H2V6.42h10v11.64Zm5-3.88h-2v-1.94h2v1.94Zm0-3.88h-2V8.36h2v1.94Z"
+                            fill="#ffffff"
+                          />
+                        </svg>
+                      )}
                       <div className="trending__movie-thumbnail-dot">·</div>
                       <div className="trending__movie-thumbnail-type">
                         {movie.movieType}
