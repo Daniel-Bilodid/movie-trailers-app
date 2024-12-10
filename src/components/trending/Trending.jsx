e73a0389d9fd77e7ab7c1b6b2b4efc07cf8c8095
Daniel <danielbeloded@gmail.com>
2024-12-10 12:53:49 +0200

    slider fix

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -73,44 +73,44 @@ const Trending = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 5, // По умолчанию
     slidesToScroll: 2,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1920,
+        breakpoint: 720,
         settings: {
-          slidesToShow: 5,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 1440,
+        breakpoint: 1084,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 2,
           slidesToScroll: 2,
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 1440,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 2,
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 1800,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
+          slidesToShow: 4,
+          slidesToScroll: 3,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 1920,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 5,
+          slidesToScroll: 3,
         },
       },
     ],
